087e580c26b4a031b6b3c91eb6a0c54a767fa8da
popoolaIbrahim12 <ibrahimpopoola926@gmail.com>
2026-02-02 16:10:10 +0100

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Section7 from './components/Section7'
 import Section8 from './components/Section8'
 import Footer from './components/Footer'
 // import Hero from './components/hero'
-import Section1 from './components/section1'
+// import Section1 from './components/section1'
 
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
     <div>
       <Header/>
      {/* <Hero/> */}
-   <Section1/>  
+   {/* <Section1/>   */}
       <Section2/>
       <Section3/>
       <Section4/>
