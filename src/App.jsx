57e06f83e8057c529d54e92e7df39697a44635de
popoolaIbrahim12 <ibrahimpopoola926@gmail.com>
2026-02-02 15:49:48 +0100

    section1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
-import Hero from './components/Hero'
-import Section1 from './components/section1'
+
 import Section2 from './components/Section2'
 import Section3 from './components/Section3'
 import Section4 from './components/Section4'
@@ -10,13 +9,15 @@ import Section6 from './components/Section6'
 import Section7 from './components/Section7'
 import Section8 from './components/Section8'
 import Footer from './components/Footer'
+import Hero from './components/hero'
+import Section1 from './components/section1'
 
 const App = () => {
   return (
     <div>
       <Header/>
-      <Hero/>
-      <Section1/>
+    <Hero/>
+     <Section1/>  
       <Section2/>
       <Section3/>
       <Section4/>
